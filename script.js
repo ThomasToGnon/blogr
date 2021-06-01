76d5fcebde54ff3b4f9d73ecb1d241bcb355f077
ThomasToGnon <82773349+ThomasToGnon@users.noreply.github.com>
2021-06-01 20:28:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+fleche = document.getElementsByClassName('arrow_light_ham');
+
+function AfficherMasquer() {
+    divInfo = document.getElementById('ul1_ham');
+
+    if (divInfo.style.display == 'none')
+        divInfo.style.display = 'block';
+        
+    else
+        divInfo.style.display = 'none';
+
+
+}
+
+
+function AfficherMasquer2(){
+    divInfoLi2 = document.getElementById('ul2_ham1');
+
+    if (divInfoLi2.style.display == 'none')
+        divInfoLi2.style.display = 'block';
+    else
+        divInfoLi2.style.display = 'none';
+}
+
+function AfficherMasquer3() {
+    divInfoLi3 = document.getElementById('ul2_ham2');
+
+    if (divInfoLi3.style.display == 'none')
+        divInfoLi3.style.display = 'block';
+    else
+        divInfoLi3.style.display = 'none';
+}
+
+function AfficherMasquer4() {
+    divInfoLi4 = document.getElementById('ul2_ham3');
+
+    if (divInfoLi4.style.display == 'none')
+        divInfoLi4.style.display = 'block';
+    else
+        divInfoLi4.style.display = 'none';
+}
+
+
+
